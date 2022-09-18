697a441dce73163a249b368d422e05526496a9d8
Ade Indriawan <aindriawan1@gmail.com>
2022-09-18 11:53:50 +0700

    Adding jwt authentication

--- controllers/auth.go
+++ controllers/auth.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 	"strconv"
+	"strings"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -115,6 +116,7 @@ func CreateToken(userId uint64) (*TokenDetails, error) {
 	os.Setenv("ACCESS_SECRET", "loremipsum")
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
 	atClaims["user_id"] = userId
 	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
@@ -154,4 +156,110 @@ func CreateAuth(userId uint64, td *TokenDetails) error {
 	}
 
 	return nil
+}
+
+type Todo struct {
+	UserID uint64 `json:"user_id"`
+	Title string `json:"title"`
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+
+	return ""
+}
+
+func VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func TokenValid(r *http.Request) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+
+	return nil
+}
+
+type AccessDetails struct {
+	AccessUuid string
+	UserId uint64
+}
+
+func ExtractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		return &AccessDetails{
+			AccessUuid: accessUuid,
+			UserId: userId,
+		}, nil
+	}
+	fmt.Println("not ok & token not valid")
+
+	return nil, err
+}
+
+func FetchAuth(authD *AccessDetails) (uint64, error) {
+	userid, err := client.Get(authD.AccessUuid).Result()
+	if err != nil {
+		return 0, err
+	}
+	userID, _ := strconv.ParseUint(userid, 10, 64)
+	return userID, nil
+}
+
+func CreateTodo(c *gin.Context) {
+	var td *Todo
+	if err := c.ShouldBindJSON(&td); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "invalid json")
+		return
+	}
+	tokenAuth, err := ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	userId, err := FetchAuth(tokenAuth)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	td.UserID = userId
+
+	// you can proceed to save the Todo to a database
+	// but we will just return it to the caller here
+	c.JSON(http.StatusCreated, td)
 }
--- controllers/menu.go
+++ controllers/menu.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/adeindriawan/itsfood-commerce/models"
@@ -45,34 +46,95 @@ func SimpleRequest5(c *gin.Context) {
 }
 
 type MenuData struct {
-	ID uint							`json:"id"`
-  Name string					`json:"name"`
-	Description string	`json:"description"`
-	VendorID uint				`json:"vendor_id"`
-  VendorName string		`json:"vendor_name"`
-	Type string 				`json:"type"`
-	RetailPrice uint 		`json:"retail_price"`
-	WholesalePrice uint	`json:"wholesale_price"`
-	PreOrderDays uint 	`json:"pre_order_days"`
-	PreOrderHours uint 	`json:"pre_order_hours"`
-	MinOrderQty uint 		`json:"min_order_qty"`
-	MaxOrderQty uint 		`json:"max_order_qty"`
-	Image string 				`json:"image"`
+	ID uint										`json:"id"`
+  Name string								`json:"name"`
+	Description string				`json:"description"`
+	VendorID uint							`json:"vendor_id"`
+  VendorName string					`json:"vendor_name"`
+	Type models.MenuCategory 	`json:"type"`
+	RetailPrice uint 					`json:"retail_price"`
+	WholesalePrice uint				`json:"wholesale_price"`
+	PreOrderDays uint 				`json:"pre_order_days"`
+	PreOrderHours uint 				`json:"pre_order_hours"`
+	MinOrderQty uint 					`json:"min_order_qty"`
+	MaxOrderQty uint 					`json:"max_order_qty"`
+	Image string 							`json:"image"`
+}
+
+type MenuDataResponse struct {
+	Data []MenuData			`json:"data"`
+	RowsCount int64			`json:"rowsCount"`
+	RowsFiltered int64	`json:"rowsFiltered"`
 }
 
 func GetMenus(c *gin.Context) {
 	var menu []MenuData
-	if err := models.DB.Table("menus m").
-							Select(
-								`m.id AS ID, m.name AS Name, m.description AS Description, v.id AS VendorID, u.name AS VendorName,
-								m.type AS Type, m.retail_price AS RetailPrice, m.wholesale_price AS WholesalePrice, m.pre_order_days AS PreOrderDays,
-								m.pre_order_hours AS PreOrderHours, m.min_order_qty AS MinOrderQty, m.max_order_qty AS MaxOrderQty, m.image AS Image`).
-							Joins("JOIN vendors v ON v.id = m.vendor_id").
-							Joins("JOIN users u ON u.id = v.user_id").
-							Limit(10).Scan(&menu).Error; err != nil {
-								c.AbortWithStatus(404)
-								fmt.Println(err)
-							} else {
-								c.JSON(200, menu)
-							}
+	var messages = []string{}
+	params := c.Request.URL.Query()
+	var typeParam, doesTypeParamExist = params["type"]
+	var lengthParam, doesLengthParamExist = params["length"]
+	var pageParam, doesPageParamExist = params["page"]
+	var searchParam, doesSearchParamExist = params["search"]
+	var sortParam, doesSortParamExist = params["sort_by"]
+	query := models.DB.Table("menus m").
+		Select(`m.id AS ID, m.name AS Name, m.description AS Description, v.id AS VendorID, u.name AS VendorName,
+		m.type AS Type, m.retail_price AS RetailPrice, m.wholesale_price AS WholesalePrice, m.pre_order_days AS PreOrderDays,
+		m.pre_order_hours AS PreOrderHours, m.min_order_qty AS MinOrderQty, m.max_order_qty AS MaxOrderQty, m.image AS Image`).
+		Joins("JOIN vendors v ON v.id = m.vendor_id").
+		Joins("JOIN users u ON u.id = v.user_id").
+		Where("m.status = ?", "Activated")
+	if doesTypeParamExist {
+		menuType := typeParam[0]
+		switch menuType {
+		case "Food", "Beverage", "Snack", "Fruit", "Grocery", "Others":
+			query = query.Where("m.type = ?", menuType)	
+		default:
+			messages = append(messages, "Parameter Type yang diberikan tidak sesuai dengan kategori menu yang ada.")
+		}
+	}
+	if doesSearchParamExist {
+		search := searchParam[0]
+		query = query.Where("m.name LIKE ?", "%" + search + "%")
+	}
+	if doesSortParamExist {
+		sort := sortParam[0]
+		query = query.Order(sort + " asc")
+	}
+	if doesLengthParamExist {
+		length, _ := strconv.Atoi(lengthParam[0])
+		query = query.Limit(length)
+	}
+	if doesPageParamExist {
+		if doesLengthParamExist {
+			page, _ := strconv.Atoi(pageParam[0])
+			length, _ := strconv.Atoi(lengthParam[0])
+			offset := (page - 1) * length
+			query = query.Offset(offset)
+		} else {
+			messages = append(messages, "Tidak ada parameter Length, maka parameter Page diabaikan.")
+		}
+	}
+	query.Scan(&menu)
+
+	rowsCount := query.RowsAffected
+	queryErr := query.Error
+
+	if queryErr != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(queryErr)
+	} else {
+		menuData := &MenuDataResponse{
+			Data: menu,
+			RowsCount: rowsCount,
+			RowsFiltered: rowsCount,
+		}
+
+		c.JSON(200, gin.H{
+			"status": "success",
+			"messages": messages,
+			"result": menuData,
+			"description": "Data menu berhasil diambil",
+		})
+		fmt.Println(rowsCount)
+	}
 }
