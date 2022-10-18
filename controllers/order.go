fe4bfc3ceaae7c57ce0db90633f520ef3da50fbd
Ade Indriawan <aindriawan1@gmail.com>
2022-10-18 19:18:49 +0700

    Added function to send telegram notification to vendors

--- controllers/order.go
+++ controllers/order.go
@@ -7,12 +7,14 @@ import (
 
 func CreateOrder(c *gin.Context) {
 	// check apakah user sudah terautentikasi dan merupakan customer
+	// check apakan user/customer tersebut berstatus aktif
 	// check apakah cart customer tersebut berisi setidaknya 1 item
 	// check apakah item di cartnya sudah memenuhi aturan min/max dan pre order
-	// apakah ada order yang mengandung ITSMINE
-	// apakah ada menu yang vendornya memiliki default delivery cost/service charge
-	// apakah ada menu yang vendornya memiliki telegram id
-	_, errorSendingTelegram := services.SendTelegram("Chat test from Gin")
+	// Tambahkan record ke tabel orders dan order details
+	// apakah ada order yang mengandung ITSMINE, jika ya, tembakkan ke API ITSMine
+	// apakah ada menu yang vendornya memiliki default delivery cost/service charge, jika ya, tambahkan record ke costs
+	// apakah ada menu yang vendornya memiliki telegram id, jika ya, kirim notifikasi telegram ke ID tersebut
+	_, errorSendingTelegram := services.SendTelegramToGroup("Chat test from Gin")
 	if errorSendingTelegram != nil {
 		c.JSON(400, gin.H{
 			"status": "failed",
--- services/telegram.go
+++ services/telegram.go
@@ -20,7 +20,7 @@ func getUrl() string {
 	return fmt.Sprintf("https://api.telegram.org/bot%s", Token)
 }
 
-func SendTelegram(text string) (bool, error) {
+func SendTelegramToGroup(text string) (bool, error) {
 	// Global variables
 	var err error
 	var response *http.Response
@@ -58,8 +58,53 @@ func SendTelegram(text string) (bool, error) {
 	}
 
 	// Log
-	log.Print("Message:", text, "was sent")
-	log.Print("Response JSON:", string(body))
+	log.Print("Message: ", text, " was sent")
+	log.Print("Response JSON: ", string(body))
+
+	// Return
+	return true, nil
+}
+
+func SendTelegramToVendor(text string, chatId string) (bool, error) {
+	// Global variables
+	var err error
+	var response *http.Response
+
+	errorLoadingEnv := godotenv.Load()
+  if errorLoadingEnv != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	ChatId = chatId
+	Token = os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	// Send the message
+	url := fmt.Sprintf("%s/sendMessage", getUrl())
+	body, _ := json.Marshal(map[string]string{
+		"chat_id": ChatId,
+		"text":    text,
+	})
+	response, err = http.Post(
+		url,
+		"application/json",
+		bytes.NewBuffer(body),
+	)
+	if err != nil {
+		return false, err
+	}
+
+	// Close the request at the end
+	defer response.Body.Close()
+
+	// Body
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return false, err
+	}
+
+	// Log
+	log.Print("Message: ", text, " was sent")
+	log.Print("Response JSON: ", string(body))
 
 	// Return
 	return true, nil
