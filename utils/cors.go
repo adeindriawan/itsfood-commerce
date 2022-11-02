715addd09cdb63774d02b9cad7474746ad455046
Ade Indriawan <aindriawan1@gmail.com>
2022-11-02 18:43:27 +0700

    Added authorization header in cors setting

--- utils/cors.go
+++ utils/cors.go
@@ -10,7 +10,7 @@ func UseCORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
